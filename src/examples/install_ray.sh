91958b052156ff99f09196a874b56d9f68f3efb6
久龙 <guyang.sgy@antfin.com>
2022-09-27 11:38:20 +0800

    fix

--- src/examples/install_ray.sh
+++ src/examples/install_ray.sh
@@ -1,4 +1,4 @@
 
-pip install ray[cpp] == 2.0.0
-mkdir ray-template && ray cpp --generate-bazel-project-template-to ray-template
+pip install ray[cpp]==2.0.0
+rm -rf ray-template && mkdir ray-template && ray cpp --generate-bazel-project-template-to ray-template
 cp -r ray-template/thirdparty ./
